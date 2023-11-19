4fa7368e23d5c014d6db25ecf0cce453526f9f39
farzana <farzanaka22@gmail.com>
2023-11-19 20:08:07 +0530

    updation 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { Routes,Route } from 'react-router-dom';
 import './App.css';
 import Header from  './components/Header.js';
-// import Footer from  './components/Footer.js';
+
 import Home from  './pages/Home.js';
 import Quiz from './pages/Quiz.js';
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Route path='quiz' element={<Quiz></Quiz>} ></Route>
      </Routes>
      
-    {/* <Footer></Footer> */}
+
     </div>
   );
 }
